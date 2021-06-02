1c120275aec92e5575971b8518507cfc334ce1ac
lvqinghao <lvqinghao@163.com>
2021-06-02 22:39:49 +0800

    memory export

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -6,7 +6,7 @@
 use anyhow::Result;
 use wasmtime::*;
 
-fn main() -> Result<()> {
+fn main1() -> Result<()> {
     // Configure the initial compilation environment, creating the global
     // `Store` structure. Note that you can also tweak configuration settings
     // with a `Config` and an `Engine` if desired.
@@ -45,7 +45,7 @@ fn main() -> Result<()> {
 }
 
 
-fn main1() -> Result<()> {
+fn main2() -> Result<()> {
     // All wasm objects operate within the context of a "store"
     let store = Store::default();
 
@@ -79,3 +79,54 @@ fn main1() -> Result<()> {
     Ok(())
 }
 
+fn main() -> Result<()> {
+    use std::str;
+
+    let store = Store::default();
+    let log_str = Func::wrap(&store, |caller: Caller<'_>, ptr: i32, len: i32| {
+        let mem = match caller.get_export("memory") {
+            Some(Extern::Memory(mem)) => mem,
+            _ => return Err(Trap::new("failed to find host memory")),
+        };
+
+        // We're reading raw wasm memory here so we need `unsafe`. Note
+        // though that this should be safe because we don't reenter wasm
+        // while we're reading wasm memory, nor should we clash with
+        // any other memory accessors (assuming they're well-behaved
+        // too).
+        unsafe {
+            let data = mem.data_unchecked()
+                .get(ptr as u32 as usize..)
+                .and_then(|arr| arr.get(..len as u32 as usize));
+            let string = match data {
+                Some(data) => match str::from_utf8(data) {
+                    Ok(s) => s,
+                    Err(_) => return Err(Trap::new("invalid utf-8")),
+                },
+                None => return Err(Trap::new("pointer/length out of bounds")),
+            };
+            assert_eq!(string, "Hello, world!");
+            println!("wasm export memory: {}", string);
+        }
+        Ok(())
+    });
+    let module = Module::new(
+        store.engine(),
+        r#"
+        (module
+            (import "" "" (func $log_str (param i32 i32)))
+            (func (export "foo")
+                i32.const 4   ;; ptr
+                i32.const 13  ;; len
+                call $log_str)
+            (memory (export "memory") 1)
+            (data (i32.const 4) "Hello, world!")
+        )
+        "#,
+    )?;
+    let instance = Instance::new(&store, &module, &[log_str.into()])?;
+    let foo = instance.get_typed_func::<(), ()>("foo")?;
+    foo.call(())?;
+
+    Ok(())
+}
