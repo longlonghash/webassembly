1e5fdd6d85716933347bc3bcceb2ad367f1ff621
lvqinghao <lvqinghao@163.com>
2021-06-03 18:38:36 +0800

    add some examples

--- cdylib-bindgen/src/lib.rs
+++ cdylib-bindgen/src/lib.rs
@@ -7,3 +7,22 @@ pub fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+/*
+
+https://docs.wasmtime.dev/wasm-rust.html
+cargo wasi build --release
+## Using cargo wasi build, there might be some errors!!!
+
+cargo build --target=wasm32-wasi --release
+cargo build --target=wasm32-unknown-unknown --release
+
+cargo +nightly build --target=wasm32-wasi --release
+cargo +nightly build --target=wasm32-unknown-unknown --release
+cargo +stable build --target=wasm32-wasi --release
+cargo +stable build --target=wasm32-unknown-unknown --release
+
+wasmtime --invoke greet ./target/wasm32-wasi/release/cdylib_bindgen.wasm "Wasmtime CLI"
+## there might be some errors!!!
+
+
+*/
